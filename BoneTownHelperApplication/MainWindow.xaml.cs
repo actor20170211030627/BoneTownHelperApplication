29397c57e7c40382411b254e50153fd1b669d6ad
actor20170211030627 <1455198886@qq.com>
2025-02-02 22:45:16 +0800

    update MemoryDllUtils.cs

--- BoneTownHelperApplication/MainWindow.xaml.cs
+++ BoneTownHelperApplication/MainWindow.xaml.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-using System.Diagnostics;
+using System.ComponentModel;
+using System.Windows.Threading;
 using BoneTownHelperApplication.Utils;
-using Memory;
 
 namespace BoneTownHelperApplication {
     /// <summary>
@@ -9,18 +9,29 @@ namespace BoneTownHelperApplication {
     /// </summary>
     public partial class MainWindow {
 
-        private const string ProcessName = "BoneTown.exe";
-        private const string Money = "0x00532A28";
-        private const string Money_Offset0 = "0x2B8";
-        private const string Money_Offset1 = "0x478";
+        private const string ProcessName = "BoneTown";
+        private const string moduleName = "BoneTown.exe";
+        private const string Money    = moduleName + "+0x00532A28,0x2B8,0x478";
+        private const string Beer     = moduleName + "+0x00532A28,0x2B8,0x5A0";
+        private const string Whiskey  = moduleName + "+0x00532A28,0x2B8,0x5A4";
+        private const string Weed     = moduleName + "+0x00532A28,0x2B8,0x5A8";
+        private const string Mushroom = moduleName + "+0x00532A28,0x2B8,0x5AC";
+        private const string Cactus   = moduleName + "+0x00532A28,0x2B8,0x5B0";
+        private const string Frog     = moduleName + "+0x00532A28,0x2B8,0x5B4";
+        private const string Crack    = moduleName + "+0x00532A28,0x2B8,0x5B8";
+        private const string XAxis    = moduleName + "+0x00532A28,0x2B8,0x7C4";
+        private const string YAxis    = moduleName + "+0x00532A28,0x2B8,0x7C8";
+        private const string ZAxis    = moduleName + "+0x00532A28,0x2B8,0x7CC";
+
+        private DispatcherTimer _timer;
         private bool ProcOpen;
-        
+
         public MainWindow() {
             InitializeComponent();
+            InitializeTimer();
 
             this.Btn_Find_Process.Click += (sender, args) => {
-                ProcessUtils.findProcess("notepad");
-                
+
                 // Mem m = new Mem();
                 // int processId = m.GetProcIdFromName(ProcessName);
                 // ProcOpen = m.OpenProcess(processId, out var failReason);
@@ -35,7 +46,67 @@ namespace BoneTownHelperApplication {
                 //     this.AT_Money.Text = failReason;
                 // }
 
+
+                /**
+                 * SharpMemoryCache
+                 */
+                // 获取目标进程
+                // var process = Process.GetProcessesByName("game").FirstOrDefault();
+                // if (process == null) return;
+                //
+                // // 创建内存操作实例
+                // MemoryCache cache = new TrimmingMemoryCache("notepad", null);
+                // // var memory = new SharpMemory(process.Id);
+                //
+                // TSource source = cache.FirstOrDefault();
+                //
+                // // 读取内存值
+                // long address = 0x12345678;
+                // int value = memory.ReadInt(address);
+                // Console.WriteLine($"读取值: {value}");
+                //
+                // // 写入内存值
+                // memory.WriteInt(address, 999);
             };
+        }
+
+        private void InitializeTimer() {
+            // 创建定时器，间隔为 1 秒
+            _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromSeconds(1);
+            _timer.Tick += Timer_Tick;
+
+            // 启动定时器（也可以在页面加载时启动）
+            _timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e) {
+            /**
+             * memory.dll
+             */
+            MemoryDllUtils.findProcess(ProcessName);
+            // 定时任务逻辑（例如更新界面控件）
+            // lblTime.Content = DateTime.Now.ToString("HH:mm:ss");
+        }
+
+        protected override void OnClosing(CancelEventArgs e) {
+            Console.WriteLine("OnClosing()");
+            // MessageBoxResult result = MessageBox.Show("是否保存更改？", "提示", MessageBoxButton.YesNoCancel);
+            // if (result == MessageBoxResult.Cancel) {
+            //     e.Cancel = true; // 取消关闭
+            //     return;
+            // }
+            // if (result == MessageBoxResult.Yes) {
+            //     // SaveData(); // 保存数据
+            // }
+            base.OnClosing(e);
+        }
+
+        protected override void OnClosed(EventArgs e) {
+            Console.WriteLine("OnClosed()");
+            //停止定时器
+            _timer.Stop();
+            base.OnClosed(e);
         }
     }
 }
---
+++ BoneTownHelperApplication/Utils/MemoryDllUtils.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using Memory;
+
+/// <summary>
+/// https://github.com/erfg12/memory.dll
+/// 32 位进程无法访问 64 位进程的模块。 
+/// </summary>
+namespace BoneTownHelperApplication.Utils {
+    public class MemoryDllUtils {
+
+        //进程id
+        protected static int ProgressId = -1;
+
+        /// <summary>打印所有进程</summary>
+        public static void printProcesses() {
+            // 获取系统中所有运行的进程
+            Process[] processes = Process.GetProcesses();
+            // 遍历并打印进程信息
+            foreach (Process process in processes) {
+                Console.WriteLine($"进程ID: {process.Id}, 进程名称: {process.ProcessName}");
+            }
+        }
+
+        /// <summary>查找进程</summary>
+        /// <param name="processName">进程名称, 例: wps</param>
+        /// <returns></returns>
+        public static Process findProcess(string processName) {
+            // 获取目标进程
+            Process targetProcess = Process.GetProcessesByName(processName).FirstOrDefault();
+            if (targetProcess == null) {
+                ProgressId = -1;
+                Console.WriteLine("目标进程未找到！");
+            } else {
+                ProgressId = targetProcess.Id;
+            }
+            return targetProcess;
+            
+                    
+            // int processId = -1;
+            // Process[] processes = Process.GetProcessesByName(processName);
+            // if (processes.Length > 0) {
+            //     processId = processes[0].Id;
+            // }
+            // byte[] buffer = new byte[4];
+            // try {
+            //     int PROCESS_ALL_ACCESS = 0x1F0FFF;
+            //     //获取缓冲区地址 :固定数组元素的不安全地址
+            //     IntPtr byteAddress = Marshal.UnsafeAddrOfPinnedArrayElement(buffer, 0);
+            //     //获取进程的最高权限
+            //     IntPtr hProcess = OpenProcess(PROCESS_ALL_ACCESS, false, processId);
+            //     //将指定内存中的值读入缓冲区 
+            //     ReadProcessMemory(hProcess, (IntPtr)baseAddress, byteAddress, 4, IntPtr.Zero);
+            //     CloseHandle(hProcess);
+            //     memoryValue = Marshal.ReadInt32(byteAddress);
+            //     return true;
+            // }
+            // catch (Exception ex) {
+            //     Console.WriteLine($"读取内存出错:{ex.Message}");
+            //     return false;
+            // }
+            // return null;
+        }
+
+        /// <summary>
+        /// 读内存值int
+        /// </summary>
+        /// <param name="moduleName">module名称, 例: wps.exe, xxx.dll</param>
+        /// <param name="code">address, module + pointer + offset, module + offset OR label in .ini file.</param>
+        /// <returns></returns>
+        public static int readInt(string code) {
+            // 创建 Memory.dll 实例
+            Mem memory = new Mem();
+            bool openSuccess = memory.OpenProcess(ProgressId);
+            if (!openSuccess) {
+                // 关闭进程句柄
+                memory.CloseProcess();
+                Console.WriteLine("打开进程失败!");
+                return -1;
+            }
+            
+            // 获取主模块的基地址
+            // IntPtr baseAddress = targetProcess.MainModule.BaseAddress;
+            // Console.WriteLine($"基地址: {baseAddress.ToString("X")}");
+            
+            // 获取模块基地址
+            // IntPtr baseAddress = memory.GetModuleAddressByName(moduleName);
+            // Console.WriteLine($"基地址: 0x{baseAddress.ToString("X")}");
+            
+            //  address 是内存地址，可以是十六进制字符串（如 "0x12345678"）或十进制值。
+            //gamedll_x64_rwdi.dll+0x00532A28,0x2B8,0x478
+            int value = memory.ReadInt(code);
+            Console.WriteLine($"读取到的值: {value}");
+            
+            //如果有多层偏移量（指针链），可以使用 memory.dll 的 GetPointerAddress 方法：
+            // int[] offsets = { 0x2B8, 0x478 }; // 多层偏移量
+            // int finalAddress = memory.GetPointerAddress(baseAddress.ToString("X"), offsets);
+            
+            // 关闭进程句柄
+            memory.CloseProcess();
+            return value;
+
+            // 修改内存值
+            // memory.WriteMemory(baseAddress.ToString("X"), "int", "999");
+            // Console.WriteLine("内存值已修改！");
+        }
+        
+        /// <summary>
+        /// 将int写入内存
+        /// </summary>
+        /// <param name="code">address, module + pointer + offset, module + offset OR label in .ini file.</param>
+        /// <param name="type">byte, 2bytes, bytes, float, int, string, double or long.</param>
+        /// <param name="value">value to write to address.</param>
+        /// <returns></returns>
+        public static bool writeInt(string code, int value) {
+            // 创建 Memory.dll 实例
+            Mem memory = new Mem();
+            bool openSuccess = memory.OpenProcess(ProgressId);
+            if (!openSuccess) {
+                // 关闭进程句柄
+                memory.CloseProcess();
+                Console.WriteLine("打开进程失败!");
+                return false;
+            }
+            
+            //  address 是内存地址，可以是十六进制字符串（如 "0x12345678"）或十进制值。
+            //gamedll_x64_rwdi.dll+0x00532A28,0x2B8,0x478
+            bool success = memory.WriteMemory(code, "int", value.ToString());
+            Console.WriteLine($"写入int: {value}, 成功: {success}");
+            
+            // 关闭进程句柄
+            memory.CloseProcess();
+            return success;
+        }
+    }
+}
