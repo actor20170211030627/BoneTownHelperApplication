ea67e8d21e2c9d0aa6011747b2e5653aefbb9151
actor20170211030627 <1455198886@qq.com>
2025-11-08 02:41:58 +0800

    add dependence: globalmousekeyhook

--- BoneTownHelperApplication/MainWindow.xaml.cs
+++ BoneTownHelperApplication/MainWindow.xaml.cs
@@ -1,10 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Globalization;
 using System.Windows;
-using System.Windows.Controls;
+using System.Windows.Forms;
+using System.Windows.Input;
 using System.Windows.Threading;
 using BoneTownHelperApplication.Utils;
+using Gma.System.MouseKeyHook;
+using Button = System.Windows.Controls.Button;
+using KeyEventArgs = System.Windows.Input.KeyEventArgs;
+using MessageBox = System.Windows.MessageBox;
 
 namespace BoneTownHelperApplication {
     /// <summary>
@@ -30,10 +36,14 @@ namespace BoneTownHelperApplication {
 
         //进程是否打开
         private bool _isProcOpen = false;
+        private bool _False = false;
+
+        private IKeyboardMouseEvents m_GlobalHook;
 
         public MainWindow() {
             InitializeComponent();
             InitializeTimer();
+            InitializeMouseKeyHook();
             
             // this.Btn_Find_Process.Click += (sender, args) => {
             //     Console.WriteLine("点击了!!!");
@@ -115,6 +125,23 @@ namespace BoneTownHelperApplication {
             //         this.TB_ZAxis.Text = s;
             //     });
             // });
+            
+            
+            /**
+             * 监听按键: 只能在窗口获取焦点时才管用
+             */
+            if (_False) {
+                this.KeyDown += delegate(object sender, KeyEventArgs e) {
+                    // 判断是否同时按下了Ctrl键和S键
+                    if (e.Key == Key.S && Keyboard.Modifiers == ModifierKeys.Control) {
+                        // 执行你的保存逻辑
+                        MessageBox.Show("保存命令被触发！");
+            
+                        // 标记事件为已处理，防止继续传递
+                        e.Handled = true;
+                    }
+                };
+            }
         }
 
         private void InitializeTimer() {
@@ -141,6 +168,166 @@ namespace BoneTownHelperApplication {
             _dispatcherTimer.Start();
         }
 
+        /// <summary>
+        /// 初始化键盘鼠标hook
+        /// </summary>
+        private void InitializeMouseKeyHook() {
+            // Note: for the application hook, use the Hook.AppEvents() instead
+            m_GlobalHook = Hook.GlobalEvents();
+            m_GlobalHook.KeyUp += GlobalHookKeyUp;
+            
+            
+            
+            //下方是组合键, 先屏蔽掉
+            if (!_False) return;
+            
+            //1. Define key combinations
+            //                                                               +数字无效???
+            // Combination combinationMoney = Combination.FromString("Control+0");
+            // Combination combinationBeer = Combination.FromString("Control+1");
+            // Key.LeftCtrl;
+            // Key.D0;
+            Combination combinationMoney = Combination.TriggeredBy(Keys.D0).With(Keys.Control);
+            Combination combinationBeer = Combination.TriggeredBy(Keys.NumPad0).Control();
+
+            //2. Define actions
+            Action actionMoney = () => {
+                // if
+                Console.WriteLine("You pressed Money");
+            };
+            Action actionBeer = () => {
+                Console.WriteLine("You Pressed Beer");
+            };
+
+            //3. Assign actions to key combinations
+            var assignment = new Dictionary<Combination, Action> {
+                {combinationMoney, actionMoney},
+                {combinationBeer, actionBeer}
+            };
+
+            //4. Install listener
+            Hook.GlobalEvents().OnCombination(assignment);
+        }
+
+        private void GlobalHookKeyUp(object sender, System.Windows.Forms.KeyEventArgs e) {
+            // KeyCode = Up, KeyData = Up, KeyValue = 38, SuppressKeyPress = False
+            // KeyCode = Down, KeyData = Down, KeyValue = 40, SuppressKeyPress = False
+            // KeyCode = Left, KeyData = Left, KeyValue = 37, SuppressKeyPress = False
+            // KeyCode = Right, KeyData = Right, KeyValue = 39, SuppressKeyPress = False
+            // Console.WriteLine($"KeyCode = {e.KeyCode}, KeyData = {e.KeyData}, KeyValue = {e.KeyValue}, SuppressKeyPress = {e.SuppressKeyPress}");
+            
+            if (e.KeyCode == Keys.Right) {  //东
+                Go2East();
+                return;
+            }
+            if (e.KeyCode == Keys.Left) {   //西
+                Go2West();
+                return;
+            }
+            if (e.KeyCode == Keys.Up) {     //北
+                Go2North();
+                return;
+            }
+            if (e.KeyCode == Keys.Down) {   //南
+                Go2South();
+                return;
+            }
+            if (e.KeyCode == Keys.H) {      //高度+
+                HeightAdd();
+                return;
+            }
+            if (e.KeyCode == Keys.L) {      //高度-
+                HeightMinus();
+                return;
+            }
+        }
+        
+
+        private void Btn_OnClick(object sender, RoutedEventArgs e) {
+            if (!(sender is Button button)) return;
+            string name = button.Name;
+            //东
+            if (name == this.Btn_XAxis_Plus.Name) {
+                Go2East();
+                return;
+            }
+            //西
+            if (name == this.Btn_XAxis_Minus.Name) {
+                Go2West();
+                return;
+            }
+            
+            //北
+            if (name == this.Btn_YAxis_Plus.Name) {
+                Go2North();
+                return;
+            }
+            //南
+            if (name == this.Btn_YAxis_Minus.Name) {
+                Go2South();
+                return;
+            }
+            
+            //高度+
+            if (name == this.Btn_ZAxis_Plus.Name) {
+                HeightAdd();
+                return;
+            }
+            //高度-
+            if (name == this.Btn_ZAxis_Minus.Name) {
+                HeightMinus();
+                return;
+            }
+        }
+
+        private void Go2East() {
+            float x = MemoryDllUtils.ReadFloat(XAxis);;
+            bool isSuccessEast = MemoryDllUtils.WriteFloat(XAxis, x + 5.0f);
+            if (!isSuccessEast) {
+                Console.WriteLine("向东+5失败!");
+            }
+        }
+        
+        private void Go2West() {
+            float x = MemoryDllUtils.ReadFloat(XAxis);
+            bool isSuccessEast = MemoryDllUtils.WriteFloat(XAxis, x - 5.0f);
+            if (!isSuccessEast) {
+                Console.WriteLine("向西+5失败!");
+            }
+        }
+        
+        private void Go2North() {
+            float y = MemoryDllUtils.ReadFloat(YAxis);
+            bool isSuccessEast = MemoryDllUtils.WriteFloat(YAxis, y + 5.0f);
+            if (!isSuccessEast) {
+                Console.WriteLine("向北+5失败!");
+            }
+        }
+        
+        private void Go2South() {
+            float y = MemoryDllUtils.ReadFloat(YAxis);
+            bool isSuccessEast = MemoryDllUtils.WriteFloat(YAxis, y - 5.0f);
+            if (!isSuccessEast) {
+                Console.WriteLine("向南+5失败!");
+            }
+        }
+        
+        private void HeightAdd() {
+            float z = MemoryDllUtils.ReadFloat(ZAxis);
+            bool isSuccessEast = MemoryDllUtils.WriteFloat(ZAxis, z + 5.0f);
+            if (!isSuccessEast) {
+                Console.WriteLine("高度+5失败!");
+            }
+        }
+        
+        private void HeightMinus() {
+            float z = MemoryDllUtils.ReadFloat(ZAxis);
+            bool isSuccessEast = MemoryDllUtils.WriteFloat(ZAxis, z - 5.0f);
+            if (!isSuccessEast) {
+                Console.WriteLine("高度-5失败!");
+            }
+        }
+        
         protected override void OnClosing(CancelEventArgs e) {
             Console.WriteLine("OnClosing()");
             // MessageBoxResult result = MessageBox.Show("是否保存更改？", "提示", MessageBoxButton.YesNoCancel);
@@ -156,71 +343,16 @@ namespace BoneTownHelperApplication {
 
         protected override void OnClosed(EventArgs e) {
             Console.WriteLine("OnClosed()");
+            
+            m_GlobalHook.KeyUp -= GlobalHookKeyUp;
+            //It is recommened to dispose it
+            m_GlobalHook.Dispose();
+            
             //停止定时器
             _dispatcherTimer.Stop();
+            // _dispatcherTimer.Tick -= ;
             MemoryDllUtils.CloseProcess();
             base.OnClosed(e);
-        }
-
-        private void Btn_OnClick(object sender, RoutedEventArgs e) {
-            if (!(sender is Button button)) return;
-            string name = button.Name;
-            //东
-            if (name == this.Btn_XAxis_Plus.Name) {
-                float x = MemoryDllUtils.ReadFloat(XAxis);;
-                bool isSuccessEast = MemoryDllUtils.WriteFloat(XAxis, x + 5.0f);
-                if (!isSuccessEast) {
-                    Console.WriteLine("向东+5失败!");
-                }
-                return;
-            }
-            //西
-            if (name == this.Btn_XAxis_Minus.Name) {
-                float x = MemoryDllUtils.ReadFloat(XAxis);
-                bool isSuccessEast = MemoryDllUtils.WriteFloat(XAxis, x - 5.0f);
-                if (!isSuccessEast) {
-                    Console.WriteLine("向西+5失败!");
-                }
-                return;
-            }
-            
-            //北
-            if (name == this.Btn_YAxis_Plus.Name) {
-                float y = MemoryDllUtils.ReadFloat(YAxis);
-                bool isSuccessEast = MemoryDllUtils.WriteFloat(YAxis, y + 5.0f);
-                if (!isSuccessEast) {
-                    Console.WriteLine("向北+5失败!");
-                }
-                return;
-            }
-            //南
-            if (name == this.Btn_YAxis_Minus.Name) {
-                float y = MemoryDllUtils.ReadFloat(YAxis);
-                bool isSuccessEast = MemoryDllUtils.WriteFloat(YAxis, y - 5.0f);
-                if (!isSuccessEast) {
-                    Console.WriteLine("向南+5失败!");
-                }
-                return;
-            }
-            
-            //高度+
-            if (name == this.Btn_ZAxis_Plus.Name) {
-                float z = MemoryDllUtils.ReadFloat(ZAxis);
-                bool isSuccessEast = MemoryDllUtils.WriteFloat(ZAxis, z + 5.0f);
-                if (!isSuccessEast) {
-                    Console.WriteLine("高度+5失败!");
-                }
-                return;
-            }
-            //高度-
-            if (name == this.Btn_ZAxis_Minus.Name) {
-                float z = MemoryDllUtils.ReadFloat(ZAxis);
-                bool isSuccessEast = MemoryDllUtils.WriteFloat(ZAxis, z - 5.0f);
-                if (!isSuccessEast) {
-                    Console.WriteLine("高度-5失败!");
-                }
-                return;
-            }
         }
     }
 }
